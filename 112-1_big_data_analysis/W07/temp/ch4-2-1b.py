4c0b2f0002523bbb1de2207bdcdc4003f39bcfb0
pyc <pychang@nkust.edu.tw>
2023-10-25 21:59:33 +0800

    move files to w06

--- 112-1_big_data_analysis/W07/temp/ch4-2-1b.py
+++ 112-1_big_data_analysis/W07/temp/ch4-2-1b.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+print("current dir: " + os.getcwd())
 
 with open("Surveys.html", "r", encoding="utf8") as fp:
     soup = BeautifulSoup(fp, "lxml")
     print(soup)
-
-import os 
-print(os.getcwd())
